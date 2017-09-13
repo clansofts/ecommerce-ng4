f25f53c6ff99da6e945e84ef5b02e470c65977ad
kanomdook <kanung.2535@gmail.com>
2017-09-13 17:19:59 +0700

    edit template

--- src/app/menu-list-x/menu-list-x.component.ts
+++ src/app/menu-list-x/menu-list-x.component.ts
@@ -7,7 +7,13 @@ import { HomeService } from '../shared/services/home-service';
   styleUrls: ['./menu-list-x.component.css']
 })
 export class MenuListXComponent implements OnInit {
-  private dataList: any = [];  
+  private dataList: any = [];
+  tiles = [
+    {text: '1', cols: 1, rows: 2, color: 'url(https://th-live.slatic.net/original/4719a77d0ec6712ba0955129a11c104b.jpg)'},    
+    {text: '2', cols: 3, rows: 1, color: 'url(https://th-live.slatic.net/original/3bc6582496c8fbda4fbcdb90ed7686a9.jpg)'},
+    {text: '3', cols: 1, rows: 1, color: 'url(https://th-live.slatic.net/original/4129e6e868701b3dd9de40671d639713.jpg)'},
+    {text: '4', cols: 2, rows: 1, color: 'url(https://th-live.slatic.net/original/25398969b95cd25a3ab686d5eeddc6ba.jpg)'},
+  ];
   constructor(private homeService:HomeService,) { }
 
   ngOnInit() {
